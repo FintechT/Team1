5bf4b4e0e31ccb0775ef9b0b5c4a3da866865a1f
FintechT <69946258+FintechT@users.noreply.github.com>
2020-08-24 20:47:46 -0500

    Update Azcapotzalco_resultado.js

--- Azcapotzalco_resultado.js
+++ Azcapotzalco_resultado.js
@@ -12,11 +12,11 @@ button.addEventListener('click', () => {
         const list = document.getElementById('list')
         var n = 0;
         for(var f=0; f>=n; f++)
-        {
+        {            
             list.innerHTML +=`<br> Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Branch.Identification}
-            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}
-            <br>Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
+            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}            
             <br> Dirección: <br> Calle:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.StreetName}
+            <br> Número: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
             <br> Alcaldía: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
             <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
             <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
