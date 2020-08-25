1dd2e138f299206f1ea9641af2b9594576f951b4
FintechT <69946258+FintechT@users.noreply.github.com>
2020-08-24 20:22:45 -0500

    Update Alvaro Obregon_resultado.js

--- Alvaro Obregon_resultado.js
+++ Alvaro Obregon_resultado.js
@@ -15,8 +15,8 @@ button.addEventListener('click', () => {
         {
             list.innerHTML +=`<br> Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Branch.Identification}
             <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}
-            <br>Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
             <br> Dirección: <br> Calle:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.StreetName}
+            <br>Número: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
             <br> Alcaldía: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
             <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
             <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
