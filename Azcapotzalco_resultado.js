3d1d9cb05e2cb2a6a01eccb20a54c957dbe4d1f4
FintechT <69946258+FintechT@users.noreply.github.com>
2020-08-24 20:49:35 -0500

    Update Azcapotzalco_resultado.js

--- Azcapotzalco_resultado.js
+++ Azcapotzalco_resultado.js
@@ -14,12 +14,7 @@ button.addEventListener('click', () => {
         for(var f=0; f>=n; f++)
         {            
             list.innerHTML +=`<br> Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Branch.Identification}
-            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}            
-            <br> Dirección: <br> Calle:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.StreetName}
-            <br> Número: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
-            <br> Alcaldía: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
-            <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
-            <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
+            IDHWIDHFHDFHFDCHADSC`
         } 
     })
     xhr.send()
