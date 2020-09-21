faf28884d1b8a49071deb7f9305fa0c2fe4c5f6f
FintechT <69946258+FintechT@users.noreply.github.com>
2020-09-21 18:47:05 -0500

    Add files via upload

---
+++ Municipios/Toluca_resultado.js
@@ -0,0 +1,26 @@
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => { 
+    let xhr
+    if(window.XMLHttpRequest) xhr = new XMLHttpRequest()
+    else xhr = new ActiveXObject("Microsfot.XMLHTTP")
+
+    xhr.open('GET', 'https://api.hsbc.com.mx/x-open-banking/v1.0/atms/country/MX/town/Toluca')
+
+    xhr.addEventListener('load', (data) => {
+        const dataJSON = JSON.parse(data.target.responseText) //cambiar stringify por parse
+        const list = document.getElementById('list')
+        var n = 0;
+        for(var f=0; f>=n; f++)
+        {
+            list.innerHTML +=`<br> Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Branch.Identification}
+            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}
+            <br>Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
+            <br> Dirección: <br> Calle:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.StreetName}
+            <br> Municipio: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
+            <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
+            <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
+        } 
+    })
+    xhr.send()
+})
