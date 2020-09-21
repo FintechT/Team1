47dc36376b2fe693c65f77fad7b577aec482903e
FintechT <69946258+FintechT@users.noreply.github.com>
2020-09-21 12:45:41 -0500

    Add files via upload

--- Huixquilucan_resultado.js
+++ Huixquilucan_resultado.js
@@ -5,26 +5,22 @@ button.addEventListener('click', () => {
     if(window.XMLHttpRequest) xhr = new XMLHttpRequest()
     else xhr = new ActiveXObject("Microsfot.XMLHTTP")
 
-    xhr.open('GET', 'https://api.hsbc.com.mx/x-open-banking/v1.0/atms/country/mx/town/IZTAPALAPA')
+    xhr.open('GET', 'https://api.hsbc.com.mx/x-open-banking/v1.0/atms/country/MX/town/Huixquilucan')
 
     xhr.addEventListener('load', (data) => {
-        const dataJSON = JSON.parse(data.target.responseText)  // cuando se use console.log(JSON.parse(data.target.responseText)) se  tendra que comentar todo este bloque
+        const dataJSON = JSON.parse(data.target.responseText) //cambiar stringify por parse
         const list = document.getElementById('list')
         var n = 0;
         for(var f=0; f>=n; f++)
         {
             list.innerHTML +=`<br> Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Branch.Identification}
-            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}            
+            <br> Sucursal:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.Site.Name}
+            <br>Número de cajero: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
             <br> Dirección: <br> Calle:  ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.StreetName}
-            <br> Número: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.BuildingNumber}
-            <br> Alcaldía: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
+            <br> Municipio: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
             <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
             <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
-        }   
+        } 
     })
     xhr.send()
 })
-
-
-
-
--- Nezahualcoyotl_resultado.js
+++ Nezahualcoyotl_resultado.js
@@ -5,10 +5,10 @@ button.addEventListener('click', () => {
     if(window.XMLHttpRequest) xhr = new XMLHttpRequest()
     else xhr = new ActiveXObject("Microsfot.XMLHTTP")
 
-    xhr.open('GET', 'https://api.hsbc.com.mx/x-open-banking/v1.0/atms/country/mx/town/IZTAPALAPA')
+    xhr.open('GET', 'https://api.hsbc.com.mx/x-open-banking/v1.0/atms/country/mx/town/Nezahualcoyotl')
 
     xhr.addEventListener('load', (data) => {
-        const dataJSON = JSON.parse(data.target.responseText)  // cuando se use console.log(JSON.parse(data.target.responseText)) se  tendra que comentar todo este bloque
+        const dataJSON = JSON.parse(data.target.responseText) //cambiar stringify por parse
         const list = document.getElementById('list')
         var n = 0;
         for(var f=0; f>=n; f++)
@@ -20,11 +20,8 @@ button.addEventListener('click', () => {
             <br> Alcaldía: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.TownName}
             <br> Ciudad: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.Country}
             <br> CP: ${dataJSON.data['0'].Brand['0'].ATM[f].Location.PostalAddress.PostCode}</br>`
-        }   
+        } 
     })
     xhr.send()
 })
-
-
-
 
